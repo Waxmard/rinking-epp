d135135b62968d5d6d644cf1f92b1ba827fc63bb
Waxmard <maxward4@gmail.com>
2026-01-17 12:55:56 -0500

    centralize error string constants

--- fastapi/app/core/auth.py
+++ fastapi/app/core/auth.py
@@ -5,6 +5,7 @@ from uuid import UUID
 from jose import JWTError, jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.constants import INVALID_CREDENTIALS_ERROR
 from app.core.security import verify_password
 from app.crud.crud_user import get_user_by_email, get_user_by_username
 from app.db.database import get_db
@@ -70,7 +71,7 @@ async def get_current_user(
 
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
+        detail=INVALID_CREDENTIALS_ERROR,
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
